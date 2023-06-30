9ef165523d2a629fd8c556353fd7c4ab77a0f999
Andrey Denisov <andrey.denisov@westwing.de>
2023-06-30 20:27:46 +0200

    add button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  padding: 8px 14px;
+  border-radius: 8px;
+  background: #F6F4FF;
+  font-size: 14px;
+  font-family: 'Inter', sans-serif;
+  font-weight: 600;
+  color: #5030E5;
+`;
